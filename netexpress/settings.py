1cbb04020c8f4e758446bb49eff73a54cde07b91
beaudelaire1 <59769164+beaudelaire1@users.noreply.github.com>
2025-02-12 00:39:17 -0300

    'le lancement de la premiere version'

--- netexpress/settings.py
+++ netexpress/settings.py
@@ -14,6 +14,8 @@ CONFIG_RECIPIENT = "vilmebeaudelaire5@gmail.com"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -116,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
