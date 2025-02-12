282335ba23cdcb52a05f5ec7f939392c22fc8bcb
beaudelaire1 <59769164+beaudelaire1@users.noreply.github.com>
2025-02-11 21:24:13 -0300

    'le lancement de la premiere version'

--- netexpress/settings.py
+++ netexpress/settings.py
@@ -10,7 +10,7 @@ CONFIG_RECIPIENT = "vilmebeaudelaire5@gmail.com"
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-print(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
