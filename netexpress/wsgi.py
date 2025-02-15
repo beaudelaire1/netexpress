050fa577dd1a9f66fffa39c98963b539fb3a75be
beaudelaire1 <59769164+beaudelaire1@users.noreply.github.com>
2025-02-15 00:11:03 -0300

    'correction du formulaire'

--- netexpress/wsgi.py
+++ netexpress/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netexpress.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netexpress.netexpress.settings')
 
 application = get_wsgi_application()
