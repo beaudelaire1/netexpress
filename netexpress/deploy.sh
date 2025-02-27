5e3a4a434d4d985570a301b11495437227bb12c5
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2025-02-27 20:02:46 -0300

    Create deploy.sh

---
+++ netexpress/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Installer les dépendances Python
+pip install -r requirements.txt
+
+# Collecter les fichiers statiques
+python manage.py collectstatic --noinput
+
+# Démarrer le serveur (par exemple, avec Gunicorn)
+gunicorn myproject.wsgi
