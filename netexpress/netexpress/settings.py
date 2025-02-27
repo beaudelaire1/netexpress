13147c343c1935b304dfd6a02c50a0aab158d698
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2025-02-27 18:46:12 -0300

    Update settings.py

--- netexpress/netexpress/settings.py
+++ netexpress/netexpress/settings.py
@@ -5,6 +5,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if not DEBUG:
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
