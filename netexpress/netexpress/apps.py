f47c47dec3fb5a0adda396d484b062d5cfe621a4
Ubuntu <ubuntu@ip-172-31-17-27.eu-north-1.compute.internal>
2025-02-27 05:18:39 +0000

    ok

--- netexpress/netexpress/apps.py
+++ netexpress/netexpress/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class LuxamaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'luxama'
+    default_auto_field = 'django.db.models.BigAutoField'  # Correction ici
