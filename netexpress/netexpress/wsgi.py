b230b41483e3d47c0620f4e0ef3bf6a58b841114
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2025-02-27 19:44:01 -0300

    Update wsgi.py

--- netexpress/netexpress/wsgi.py
+++ netexpress/netexpress/wsgi.py
@@ -1,17 +1,12 @@
-"""
-WSGI config for netexpress project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netexpress.settings')
 
-from whitenoise import WhiteNoise
+# Définition correcte de la variable application
+application = get_wsgi_application()
+
+# Ajout de WhiteNoise après avoir défini application
 application = WhiteNoise(application)
+
