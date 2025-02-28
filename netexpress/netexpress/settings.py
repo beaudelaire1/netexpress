33784935c22bc3bd3ec966b67a03d83d1934baea
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2025-02-27 21:27:27 -0300

    Update settings.py

--- netexpress/netexpress/settings.py
+++ netexpress/netexpress/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
-DEBUG = True
+DEBUG = False
 
 if not DEBUG:
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
