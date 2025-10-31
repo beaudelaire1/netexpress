64b312f53ca751b1311998c36f1385e59862ac4f
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2025-10-31 13:38:05 -0300

    Delete nettoyage_express_ancien directory

--- nettoyage_express_ancien/netexpress/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Installer les dépendances Python
-pip install -r requirements.txt
-
-# Collecter les fichiers statiques
-python manage.py collectstatic --noinput
-
-# Démarrer le serveur (par exemple, avec Gunicorn)
-gunicorn netexpress.wsgi
