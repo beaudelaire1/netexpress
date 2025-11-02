e53ee18d144104080c303128170951e36b8a799e
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2025-11-02 16:46:13 -0300

    images test solve

--- netexpress/netexpress/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class LuxamaConfig(AppConfig):
-    name = 'luxama'
-    default_auto_field = 'django.db.models.BigAutoField'  # Correction ici
--- netexpress/netexpress/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('gestion/', admin.site.urls),
-    path('', include('luxama.urls')),
-]
